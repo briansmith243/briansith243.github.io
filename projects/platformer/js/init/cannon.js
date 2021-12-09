89a72880bf65d2a11d9c64ded0c7c57c202e817d
briansmith243 <brismi243@gmail.com>
2021-12-09 15:24:48 +0000

    Added multiple cannons/ added platforms to jump on/ I added players that you need to collect

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,47 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     *
+     * GOAL: Add at least 3 cannons to make your level challenging. 
+     * 
+     * Use the createCannon Function to create cannons for the level. 
+     * 
+     * createCannon() takes these arguments:
+     *      
+     *      createCannon(type, position, delay);
+     * 
+     *      type: "top", "bottom", "left", or "right"
+     *      position: The position of the cannon along whichever side the cannon is placed on
+     *          - the x coordinate for "top" and "bottom" cannons
+     *          - the y coordinate for "left" and "right" cannons
+     *      delay: OPTIONAL the number of milliseconds to wait before firing the first projectile
+     */ 
+    function init(game) {
+        let createCannon = cannon.create;
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        /*
+createCannon(type, position, delay);
+
+type: The type of cannon to be made. This can be one of the following Strings: "top", "bottom", "left", or "right"
+position: The position coordinate for the cannon along the wall it is placed.
+delay: OPTIONAL The number of milliseconds to wait before firing the first projectile
+
+*/
+
+
+        // example: 
+        createCannon("top", 450); // a cannon on the top of the screen, located at x = 450
+        createCannon("left", 300, 1000); // a cannon on the left side of the screen, located at y = 300, with a 1 second delay (1000ms)
+        createCannon("right", 160, 180);
+        createCannon("bottom",500, 100);
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+    cannon.init = init;
+})(window);
